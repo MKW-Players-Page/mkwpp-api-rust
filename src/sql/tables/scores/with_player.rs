06932025c378da239fbef9dbc9fd2f8ce3e2cdb5
FallBackITA27 <diegomassanova4@gmail.com>
2025-03-08 23:48:20 +0100

    rename table names in a macro

--- src/sql/tables/scores/with_player.rs
+++ src/sql/tables/scores/with_player.rs
@@ -54,10 +54,10 @@ impl BasicTableQueries for ScoresWithPlayer {
     async fn select_star_query(
         executor: &mut sqlx::PgConnection,
     ) -> Result<Vec<sqlx::postgres::PgRow>, sqlx::Error> {
-        return sqlx::query(&format!(
-            "SELECT {0}.id AS s_id, value, category, is_lap, track_id, date, video_link, ghost_link, comment, initial_rank, {1}.id, name, alias, region_id FROM {0} LEFT JOIN {1} ON {0}.player_id = {1}.id;",
-            super::Scores::TABLE_NAME,
-            PlayersBasic::TABLE_NAME,
+        return sqlx::query(const_format::formatc!(
+            "SELECT {scores_table}.id AS s_id, value, category, is_lap, track_id, date, video_link, ghost_link, comment, initial_rank, {players_table}.id, name, alias, region_id FROM {scores_table} LEFT JOIN {players_table} ON {scores_table}.player_id = {players_table}.id;",
+            scores_table = super::Scores::TABLE_NAME,
+            players_table = PlayersBasic::TABLE_NAME,
         ))
         .fetch_all(executor)
         .await;
@@ -200,49 +200,52 @@ impl ScoresWithPlayer {
                     SELECT *
                     FROM (
                         SELECT
-                            {0}.id AS s_id,
-                            {0}.value,
-                            {0}.category,
-                            {0}.is_lap,
-                            {0}.track_id,
+                            {scores_table}.id AS s_id,
+                            {scores_table}.value,
+                            {scores_table}.category,
+                            {scores_table}.is_lap,
+                            {scores_table}.track_id,
                             ROW_NUMBER() OVER(
-                                PARTITION BY {0}.track_id, {0}.is_lap
-                                ORDER BY {0}.value ASC, {3}.value ASC
+                                PARTITION BY {scores_table}.track_id, {scores_table}.is_lap
+                                ORDER BY {scores_table}.value ASC, {standard_level_table}.value ASC
                             ) AS row_n,
                             date,
                             video_link,
                             ghost_link,
                             comment,
                             initial_rank,
-                            {1}.id,
-                            COALESCE({3}.code, 'NW') AS code,
+                            {players_table}.id,
+                            COALESCE({standard_level_table}.code, 'NW') AS code,
                             name,
                             alias,
-                            region_id FROM {0}
-                        LEFT JOIN {1} ON
-                            {0}.player_id = {1}.id
-                        LEFT JOIN {2} ON
-                            {0}.track_id = {2}.track_id AND
-                            {0}.value <= {2}.value AND
-                            {2}.category <= {0}.category AND
-                            {2}.is_lap = {0}.is_lap
-                        LEFT JOIN {3} ON
-                            {3}.id = {2}.standard_level_id
+                            region_id FROM {scores_table}
+                        LEFT JOIN {players_table} ON
+                        {scores_table}.player_id = {players_table}.id
+                        LEFT JOIN {standards_table} ON
+                            {scores_table}.track_id = {standards_table}.track_id AND
+                            {scores_table}.value <= {standards_table}.value AND
+                            {standards_table}.category <= {scores_table}.category AND
+                            {standards_table}.is_lap = {scores_table}.is_lap
+                        LEFT JOIN {standard_level_table} ON
+                            {standard_level_table}.id = {standards_table}.standard_level_id
                         WHERE
-                            {0}.category <= $1 AND
-                            {0}.date <= $2 AND
-                            {1}.region_id = ANY($3) 
-                            {4}
-                        ORDER BY value ASC, {3}.value ASC
+                            {scores_table}.category <= $1 AND
+                            {scores_table}.date <= $2 AND
+                            {players_table}.region_id = ANY($3) 
+                            {is_lap_where}
+                        ORDER BY value ASC, {standard_level_table}.value ASC
                     ) WHERE row_n = 1
                 ) ORDER BY track_id ASC, is_lap ASC;
                 "#,
-            super::Scores::TABLE_NAME,
-            PlayersBasic::TABLE_NAME,
-            crate::sql::tables::standards::Standards::TABLE_NAME,
-            crate::sql::tables::standard_levels::StandardLevels::TABLE_NAME,
-            if is_lap.is_some() {
-                "AND is_lap = $4"
+            scores_table = super::Scores::TABLE_NAME,
+            players_table = PlayersBasic::TABLE_NAME,
+            standards_table = crate::sql::tables::standards::Standards::TABLE_NAME,
+            standard_level_table = crate::sql::tables::standard_levels::StandardLevels::TABLE_NAME,
+            is_lap_where = if is_lap.is_some() {
+                const_format::formatc!(
+                    "AND {scores_table}.is_lap = $4",
+                    scores_table = super::Scores::TABLE_NAME,
+                )
             } else {
                 ""
             }
