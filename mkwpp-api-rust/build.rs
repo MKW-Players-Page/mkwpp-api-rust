f19008866cb90b0cb43747a1152fc6917aeec2b3
FallBackITA27 <diegomassanova4@gmail.com>
2025-03-15 09:57:24 +0100

    re-order complete crate

--- mkwpp-api-rust/build.rs
+++ mkwpp-api-rust/build.rs
@@ -1,5 +1,12 @@
+use std::{env::set_current_dir, io::Write};
+
 fn main() -> Result<(), anyhow::Error> {
+    // build.rs sets current dir to its own directory
+    set_current_dir("..")?;
+
     run_migrations()?;
+    sql_env_valid()?;
+
     // Do stuff to generate docs here
 
     return Ok(());
@@ -8,21 +15,20 @@ fn main() -> Result<(), anyhow::Error> {
 fn run_migrations() -> Result<(), anyhow::Error> {
     let migrations = std::process::Command::new("sqlx")
         .args(["database", "setup", "--source", "db/migrations"])
-        .output()
-        .expect("Failed to run migrations");
+        .output()?;
     if !migrations.status.success() {
         anyhow::bail!("Couldn't run SQLX");
     }
-    return Ok(()); 
+    return Ok(());
 }
 
 fn sql_env_valid() -> Result<(), anyhow::Error> {
-    let migrations = std::process::Command::new("sqlx")
-        .args(["database", "setup", "--source", "db/migrations"])
-        .output()
-        .expect("Failed to run migrations");
-    if !migrations.status.success() {
-        anyhow::bail!("Couldn't run SQLX");
+    if !std::path::Path::new("./.env").is_file(){
+        let mut file = std::fs::File::create("./.env")?;
+        file.write_all("".as_bytes())?;
     }
-    return Ok(()); 
+    
+    
+
+    return Ok(());
 }
