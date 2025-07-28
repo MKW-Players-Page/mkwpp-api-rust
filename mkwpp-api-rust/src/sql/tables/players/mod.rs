88f38b68811b4dc492d5bf9c41f15964b7fb6400
falb <diegomassanova4@gmail.com>
2025-07-28 19:17:54 +0200

    yml test

--- mkwpp-api-rust/src/sql/tables/players/mod.rs
+++ mkwpp-api-rust/src/sql/tables/players/mod.rs
@@ -91,6 +91,13 @@ impl Players {
         .map_err(|e| EveryReturnedError::GettingFromDatabase.into_final_error(e));
     }
 
+    pub async fn asd() {
+        
+        println!("INSANELY LONG LINE THAT I WILL REFUSE TO FIX ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD ASD")
+        
+        
+    }
+
     pub async fn update_player_alias(
         executor: &mut sqlx::PgConnection,
         player_id: i32,
